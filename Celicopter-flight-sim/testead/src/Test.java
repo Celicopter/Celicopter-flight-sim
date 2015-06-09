e147be169074e1436d179f432ca4686246c654e7
Nathan Schilling <nschilling10@gmail.com>
2015-06-08 20:14:11 -0400

    Brought ScreenObject more up-to-date and started playing around with some of it's methods. Tested my methodology for creating a polygon and fixed an array out of bound dexception it was causing. Created a new project called Test for testing things

---
+++ Celicopter-flight-sim/testead/src/Test.java
@@ -0,0 +1,84 @@
+import java.awt.*;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+
+import javax.swing.JFrame;
+
+public class Test extends Canvas{
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	public Rectangle virtualBounds;
+
+	public static void main(String []args){
+		Test t=new Test();
+		JFrame f=new JFrame();
+
+		/*
+		//Gets information related to the display screen. This only works for single-monitor devices
+		Toolkit tk = Toolkit.getDefaultToolkit();  
+
+		//Gets the width of the screen and stores it in variable xSize
+	    int xSize = ((int) tk.getScreenSize().getWidth()); 
+
+	    //Gets the height of the screen and stores it in variable ySize
+	    int ySize = ((int) tk.getScreenSize().getHeight());  
+
+	    //Sets the screen to it's maximum allowable size (maximizes screen)
+	    f.setSize(xSize,ySize);
+		 */
+
+		/**This code works for devices with multiple screens*/
+		
+		
+		f.setBounds(t.virtualBounds);
+		f.add(t);
+		f.setVisible(true);
+		f.addWindowListener(new WindowAdapter() {
+			public void windowClosing(WindowEvent e) {
+				System.exit(0);
+			}//closes window listener that closes the program when we exit out of the window
+		}//closes patch
+		);
+	}
+
+	public Test() {
+		super();
+		//Defines a variable to hold the maximum bounds of the screen we will find shortly
+		virtualBounds = new Rectangle();
+		//Gets an object that encapsulates all periphery devices attached to this computer (screens, printers, etc.)
+		GraphicsEnvironment ge = GraphicsEnvironment.getLocalGraphicsEnvironment();
+		//Unpacks this object and gets a list of all peripheries 
+		GraphicsDevice[] gs = ge.getScreenDevices();
+		//Iterates through this list, going to each device, getting the list of settings with each device, and parsing the maximum window size from these settings  
+		for (int j = 0; j < gs.length; j++) {
+			//Gets the list of settings associated with the device the for loop is currently on
+			GraphicsConfiguration[] gc =gs[j].getConfigurations();
+			//Iterates through the settings to get the bounds
+			for (int i=0; i < gc.length; i++) {
+				virtualBounds =virtualBounds.union(gc[i].getBounds());
+			}
+		}
+		System.out.println(virtualBounds);
+	}
+	
+	public void paint(Graphics g){
+		g.fillPolygon(createPoly(100,10));
+	}
+	
+	private Polygon createPoly(int numberOfSides,int sideLength){
+		int[] xPts=new int[numberOfSides];
+		int[] yPts=new int[numberOfSides];
+		double intAngle=2*Math.PI/numberOfSides;
+		double r=sideLength/(2*Math.sin(intAngle));
+		double theta=(3*Math.PI/2)+intAngle/2;
+		for(int i=0;i<numberOfSides;i++){
+			xPts[i]=(int) (r*Math.sin(theta+i*intAngle));
+			yPts[i]=(int) (r*Math.cos(theta+i*intAngle));
+		}
+		Polygon p= new Polygon(xPts,yPts,numberOfSides);
+		p.translate((int) (0+p.getBounds2D().getWidth()/2), (int) (0+p.getBounds2D().getHeight()/2));
+		return p;
+	}
+}
